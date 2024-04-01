4da639cf7a727abe27a851645b48800b1912d568
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2024-04-01 14:48:38 -0300

    refactor: add secret do jwt ao .env

--- src/infra/adapter/JwtAdapterImpl.ts
+++ src/infra/adapter/JwtAdapterImpl.ts
@@ -1,9 +1,11 @@
 import jsonwebtoken from 'jsonwebtoken'
 import JwtAdapter from '../../application/adapter/JwtAdapter'
+import { config } from 'dotenv'
+config()
 
 export default class JwtAdapterImpl implements JwtAdapter {
   sign(data: object) {
-    return jsonwebtoken.sign(data, 'secret', {
+    return jsonwebtoken.sign(data, String(process.env.SECRET_JWT), {
       expiresIn: '1d',
     })
   }
