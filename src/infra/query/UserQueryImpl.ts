20010c5ef8c63468e1676d798cca75b10e5ec798
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2024-04-01 18:33:29 -0300

    feat: implementação do user query

---
+++ src/infra/query/UserQueryImpl.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-useless-constructor */
+
+import { DataSource, Repository } from 'typeorm'
+import UserEntity from '../database/entity/UserEntity'
+import UserQuery from '../../application/query/UserQuery'
+import Id from '../../domain/Id'
+
+export default class UserQueryImpl implements UserQuery {
+  private userEntityRepository: Repository<UserEntity>
+
+  constructor(databaseConnection: DataSource) {
+    this.userEntityRepository = databaseConnection.getRepository(UserEntity)
+  }
+
+  async getById(userId: Id) {
+    const userEntity = await this.userEntityRepository.findOneBy({
+      id: userId.getValue(),
+    })
+    if (!userEntity) return null
+    return {
+      userId: userEntity.id,
+      username: userEntity.username,
+      email: userEntity.email,
+      profilePicture: userEntity.profilePictureUri,
+    }
+  }
+}
