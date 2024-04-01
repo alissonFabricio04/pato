03d2fdcd91bd4d77296c741a8eac3ae3f7253101
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2024-04-01 14:47:55 -0300

    refactor: mudando variavel de ambiente de PORT para API_PORT

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata'
 import express from 'express'
 import router from './infra/router'
+import { config } from 'dotenv'
+config()
 
 const app = express()
 app.use(express.json())
 
 app.use(router)
 
-const port = process.env.PORT || 8000
+const port = process.env.API_PORT || 8000
 app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`)
 })
